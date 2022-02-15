7f6c8241e4116a257db76af4fc6fd1ce858c6ba6
talashraf <talashraf@snelstart.nl>
2022-02-15 09:13:21 +0100

    class

--- Boekhouden/Program.cs
+++ Boekhouden/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Boekhouden
 {
@@ -6,7 +10,60 @@ namespace Boekhouden
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            var roots = RootObject();
+            if (roots != null)
+            {
+                
+
+                foreach (var root in roots)
+                {
+                   
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("TableNumber: " + root.TableNumber + " ");
+                    Console.WriteLine("SubTotal: " + root.SubTotal);
+                    Console.WriteLine("CustomerDiscount: " + root.CustomerDiscount.DiscountAmount);
+                    var totaal = root.SubTotal - root.CustomerDiscount.DiscountAmount;
+                    Console.WriteLine("Total: " + totaal);
+                    Console.WriteLine("OrderDateTime: " + root.OrderDateTime);
+
+                    foreach (var row in root.TransactionRows)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("ProductDescription: " + row.ProductDescription);
+                        Console.WriteLine("Price: " + row.Price);
+                        Console.WriteLine("VatType: " + row.VatType);
+                        Console.WriteLine("VatAmount: " + row.VatAmount);
+                        Console.WriteLine("TransactionRowDiscount: " + row.TransactionRowDiscount);
+                        Console.WriteLine("-------------------");
+                    }
+
+                    var prijsTotal = root.TransactionRows.Sum(tr => tr.Price);
+                    var prijsMinDiscount = root.TransactionRows.Sum(tr => tr.Price - tr.TransactionRowDiscount);
+                    var differenceSubTotal = root.SubTotal - prijsMinDiscount;
+
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine("Total: " + prijsTotal);
+                    Console.WriteLine("Total(Min Discount): " + prijsMinDiscount);
+                    Console.WriteLine("Difference: " + differenceSubTotal);
+                }
+            }
+            Console.ReadKey();
+        }
+
+
+        static List<Root> RootObject()
+        {
+            string Json = (@"C:\Users\talashraf\OneDrive - SnelStart Software B.V\Bureaublad\C#\Boekhouden\Boekhouden\json\inputData.json");
+            if (File.Exists(Json))
+            {
+                var transactionRow = JsonConvert.DeserializeObject<List<Root>>(File.ReadAllText(Json));
+                //string json = System.Text.Json.JsonSerializer.Serialize(transactionRow.ToArray());
+
+                //File.WriteAllText(Json, json);
+                return transactionRow;
+
+            }
+            return null;
         }
     }
 }
