8428f1e3bbf6f5b50d1b4c8ea465e2234c73d7fe
talashraf <talashraf@snelstart.nl>
2022-02-15 21:34:22 +0100

    avond

--- Boekhouden/Program.cs
+++ Boekhouden/Program.cs
@@ -1,23 +1,88 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Boekhouden
 {
     internal class Program
     {
+        public void Database()
+        {
+            
+            var datasource = @"(localdb)\MSSQLLocalDB";//your server
+            var database = "snelstart"; //your database name
+            var username = ""; //username of server to connect
+            var password = ""; //password
+            string connString = @"Data Source=" + datasource + ";Initial Catalog="
+            + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    Console.WriteLine("Openning Connection ...");
+                    //open connection
+                    conn.Open();
+                    Console.WriteLine("Connection successful!");
+                    //create a new SQL Query using StringBuilder
+                    StringBuilder strBuilder = new StringBuilder();
+                    Console.WriteLine("Wat is je naam?");
+                    string insert1 = Console.ReadLine();
+                    strBuilder.Append("INSERT INTO student (fname, lname, gender) VALUES ");
+                    strBuilder.Append($"('{insert1}', N'console',N'male'),");
+                    string sqlQuery = strBuilder.ToString();
+                    using (SqlCommand command = new SqlCommand(sqlQuery, conn)) //pass SQL query created above and connection
+                    {
+                        command.ExecuteNonQuery(); //execute the Query
+                        Console.WriteLine("Query Executed.");
+                    }
+                    strBuilder.Clear(); // clear all the string
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
         static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            p1.InLezen();
+            p1.Database();
+
+
+
+            Console.ReadKey();
+        }
+
+
+        static List<Root> RootObject()
+        {
+            string Json = (@"C:\Users\talashraf\OneDrive - SnelStart Software B.V\Bureaublad\C#\Boekhouden\Boekhouden\json\inputData.json");
+            if (File.Exists(Json))
+            {
+                var transactionRow = JsonConvert.DeserializeObject<List<Root>>(File.ReadAllText(Json));
+                //string json = System.Text.Json.JsonSerializer.Serialize(transactionRow.ToArray());
+                //File.WriteAllText(Json, json);
+                return transactionRow;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void InLezen()
         {
             var roots = RootObject();
             if (roots != null)
             {
-                
-
                 foreach (var root in roots)
                 {
-                   
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.WriteLine("TableNumber: " + root.TableNumber + " ");
                     Console.WriteLine("SubTotal: " + root.SubTotal);
@@ -47,23 +112,6 @@ namespace Boekhouden
                     Console.WriteLine("Difference: " + differenceSubTotal);
                 }
             }
-            Console.ReadKey();
-        }
-
-
-        static List<Root> RootObject()
-        {
-            string Json = (@"C:\Users\talashraf\OneDrive - SnelStart Software B.V\Bureaublad\C#\Boekhouden\Boekhouden\json\inputData.json");
-            if (File.Exists(Json))
-            {
-                var transactionRow = JsonConvert.DeserializeObject<List<Root>>(File.ReadAllText(Json));
-                //string json = System.Text.Json.JsonSerializer.Serialize(transactionRow.ToArray());
-
-                //File.WriteAllText(Json, json);
-                return transactionRow;
-
-            }
-            return null;
         }
     }
 }
